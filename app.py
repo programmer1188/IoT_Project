b6d337dc006a42b6d4fb02da407bfaa5fb3f236c
programmer1188 <72691700+programmer1188@users.noreply.github.com>
2020-10-22 10:56:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def predict():
         
         prediction=model.predict([[Temperature,Humidity,SoilMoisture]])
         output=prediction[0]
-        if output==0:
+        if output<1:
             return render_template('index.html',prediction_text="Turn off the Motor")
         else:
             return render_template('index.html',prediction_text="Turn on the Motor".format(output))
